183fff9a1419634690e0f452f821d5413ccb45d1
Muhammad Uzair Arslan <uzairarslan@quantomex.com>
2025-03-06 05:07:18 +0500

    DIsabled the secondary hero component social links

--- src/Components/SecondaryHero.js
+++ src/Components/SecondaryHero.js
@@ -11,23 +11,6 @@ export default function SecondaryHero({ bg, page, mainHeading }) {
           Home / <span>{page}</span>
         </p>
         <h1 className="secondaryHeroMainHeading">{mainHeading}</h1>
-        {/* <div className="secondaryHeroSocialIconWrapper">
-          <div className="socialIconWrapper">
-            <img src={youtube} alt="Youtube Social Link" />
-          </div>
-          <div className="socialIconWrapper">
-            <img src={tiktok} alt="Tiktok Social Link" />
-          </div>
-          <div className="socialIconWrapper">
-            <img src={linkedin} alt="LinkedIn Social Link" />
-          </div>
-          <div className="socialIconWrapper">
-            <img src={instagram} alt="Instagram Social Link" />
-          </div>
-          <div className="socialIconWrapper">
-            <img src={facebook} alt="Facebook Social Link" />
-          </div>
-        </div> */}
       </div>
     </section>
   );
